abe27a37e00f5506892ff9d3835f0eef96c3e84a
Tomasz <akyrGit@gmail.com>
2017-06-10 21:53:24 +0200

    Few visual updates.

--- src/com/HowToLearnEnglishWords/guiWordsLearn.java
+++ src/com/HowToLearnEnglishWords/guiWordsLearn.java
@@ -1,6 +1,8 @@
 package com.HowToLearnEnglishWords;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.text.Caret;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,7 +25,7 @@ public class guiWordsLearn extends JFrame implements ActionListener {
     private JButton resetLearningButton;
     private JLabel howManyWordsLeftLabel;
     private JTextField howManyWordsLeftTextField;
-
+    Color colorScore = new Color(0, 128, 0);
 
     private actionsWordsLearn word;
     private int tmpPoints = 1;
@@ -34,8 +36,9 @@ public class guiWordsLearn extends JFrame implements ActionListener {
 
     public guiWordsLearn() throws FileNotFoundException {
         word = new actionsWordsLearn();
-        setResizable(true);
+        //setResizable(true);
         setVisible(true);
+
 checkSum();
         makeGUI();
         pack();
@@ -48,61 +51,94 @@ checkSum();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        Sets app title
         this.setTitle("WordsLearn");
-
+       // this.getContentPane().setBackground(Color.WHITE);
         setLayout(new GridLayout(6, 1));
 
+        Font font  = new Font("SansSerif", Font.BOLD, 17);
 //      Set buttons for including a file paths
         pathEngButton = new JButton("1. First path");
+        pathEngButton.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        pathEngButton.setFont(font);
         pathEngButton.setToolTipText("Include path to first set of words file, please.");
         pathEngButton.addActionListener(this);
         add(pathEngButton);
 
         pathPolButton = new JButton("2. Second path");
+        pathPolButton.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        pathPolButton.setFont(font);
         pathPolButton.setToolTipText("Include path to second set of words file, please.");
         pathPolButton.addActionListener(this);
         add(pathPolButton);
 
 //        A button for start learning
         startLearningButton = new JButton("Start learning");
+        startLearningButton.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        startLearningButton.setFont(font);
         startLearningButton.addActionListener(this);
         add(startLearningButton);
 
         resetLearningButton = new JButton("Reset learning");
-        resetLearningButton.addActionListener(this);
+        resetLearningButton.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        resetLearningButton.setFont(font);
         resetLearningButton.addActionListener(this);
         add(resetLearningButton);
 
-        yourRandomWordLabel = new JLabel("Your random word: ");
+        yourRandomWordLabel = new JLabel("Your random word: ",SwingConstants.CENTER);
+       // yourRandomWordLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        yourRandomWordLabel.setFont(font);
         add(yourRandomWordLabel);
 
-        translatePleaseLabel = new JLabel("Translate please: ");
+        translatePleaseLabel = new JLabel("Translate please: ",SwingConstants.CENTER);
+       // translatePleaseLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        translatePleaseLabel.setFont(font);
         add(translatePleaseLabel);
 
 //        Text field for show a result of a words rand from a text file
         randomScoreField = new JTextField();
+        randomScoreField.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        Color c = new Color(159, 251, 136);
+        randomScoreField.setBackground(c);
+        randomScoreField.setHorizontalAlignment(SwingConstants.CENTER);
         randomScoreField.setText(" ");
+        randomScoreField.setFont(font);
         randomScoreField.setEditable(false);
         add(randomScoreField);
 
 //        Text field for input ansfer by a user
         inputWordTextField = new JTextField();
         inputWordTextField.setToolTipText("Type your translation, then press ENTER. ");
+        inputWordTextField.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        inputWordTextField.setHorizontalAlignment(SwingConstants.CENTER);
+        inputWordTextField.setFont(font);
+        inputWordTextField.setBackground(Color.ORANGE);
+        inputWordTextField.setForeground(Color.BLACK);
         inputWordTextField.addActionListener(this);
         add(inputWordTextField);
 
 //        Score field
 
         scoreLabel = new JLabel("Score: ");
+       // scoreLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+
+      //  scoreLabel.setForeground(colorScore);
+        scoreLabel.setFont(font);
         add(scoreLabel);
 
         yourScoreTextField = new JTextField();
+       // yourScoreTextField.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        yourScoreTextField.setFont(font);
         yourScoreTextField.setEditable(false);
         add(yourScoreTextField);
 
-        howManyWordsLeftLabel = new JLabel("This word will repeated ");
+        howManyWordsLeftLabel = new JLabel("This word will repeated: ");
+       // howManyWordsLeftLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        howManyWordsLeftLabel.setFont(font);
         add(howManyWordsLeftLabel);
 
         howManyWordsLeftTextField = new JTextField();
+       // howManyWordsLeftTextField.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        howManyWordsLeftTextField.setHorizontalAlignment(SwingConstants.CENTER);
+        howManyWordsLeftTextField.setFont(font);
         howManyWordsLeftTextField.setEditable(false);
         add(howManyWordsLeftTextField);
 
@@ -129,12 +165,15 @@ checkSum();
                 doRandom();
                 showDrawnWords();
                 howManyWordsLeftTextField.setText(word.rememberWordRepetition.get(word.k) + " more times");
+                howManyWordsLeftTextField.setHorizontalAlignment(SwingConstants.CENTER);
 
             } else if (sum > 0 && (word.rememberWordApperance() == 0)) {
                 doRandom();
                 showDrawnWords();
                 setPointsIcon();
                 howManyWordsLeftTextField.setText(word.rememberWordRepetition.get(word.k) + " ");
+                howManyWordsLeftTextField.setHorizontalAlignment(SwingConstants.CENTER);
+
 
 
             } else if (sum == 0 /*&& (word.rememberWordApperance() == 0)*/) {
@@ -244,7 +283,8 @@ checkSum();
 
     public void setPointsIcon() {
         if (word.points == tmpPoints) {
-            scoreLabel.setForeground(Color.GREEN);
+           // scoreLabel.setForeground(Color.GREEN);
+            scoreLabel.setForeground(colorScore);
             scoreLabel.setText("Score: " + word.getPoints() + "/" + word.getAllTests());
             tmpPoints = word.points;
             tmpPoints++;
